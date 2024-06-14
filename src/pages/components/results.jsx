a1376b3ca1daca3fb863dd9bb8b2bb5cc2d8a188
kenwilde1 <kenwilde@TWRH2JKNQK.local>
2024-06-14 23:58:20 +0100

    first

---
+++ src/pages/components/results.jsx
@@ -0,0 +1,70 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import db from "../../../utils/firestore"
+import { collection, getDocs } from "firebase/firestore"
+
+import { fetchTally } from '../../../utils/calculate_scores';
+
+import gold from '../../images/gold.svg';
+import silver from '../../images/silver.svg';
+import bronze from '../../images/bronze.svg';
+import normal from '../../images/normal.png';
+
+const Medal = ({ position }) => {
+  console.log(position);
+  if (position === 0) {
+    return <img src={gold.src} />
+  } else if (position === 1) {
+    return <img src={silver.src} />
+  } else if (position === 2) {
+    return <img src={bronze.src} />
+  } else {
+    return <img src={normal.src} />
+  }
+}
+
+
+const Scores = (scores) => {
+  return (
+    <ol>
+      {scores.scores
+        .sort((a, b) => b.score - a.score)
+        .map((player, index) => {
+          const score = player.score === 0 ? 'N/A' : player.score
+          console.log(player.name, index);
+          return (
+          <li className="leaderboard-position">
+            <div className="leaderboard-name">
+              <span className="medal"><Medal position={index} /></span>
+              <span className="leaderboard-name">{player.name}</span>
+            </div>
+            <span className="leaderboard-score font-bold">{score}</span>
+            </li>
+          );
+        })}
+    </ol>
+  )
+}
+
+const ListItems = () => {
+  const [scores, setScores] = useState([])
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const querySnapshot = await getDocs(collection(db, "results"));
+      console.log(querySnapshot);
+      const items = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+      setScores(fetchTally(items))
+    }
+    fetchItems()
+  }, [])
+
+  return (
+    <div className="border w-96 text-center p-4 leaderboard">
+      <Scores scores={scores} />
+    </div>
+  )
+}
+
+export default ListItems
---
+++ utils/calculate_scores.js
@@ -0,0 +1,66 @@
+import hughPredictions from '../data/hugh';
+import philipPredictions from '../data/philip';
+import stephenPredictions from '../data/stephen';
+import kennyPredictions from '../data/kenny';
+import dylWPredictions from '../data/dyl_w';
+import shanePredictions from '../data/shane';
+import alanPredictions from '../data/alan';
+
+const getWinner = (result) => {
+    if (result.homeGoals > result.awayGoals) {
+        return result.home;
+    } else if (result.homeGoals < result.awayGoals) {
+        return result.away;
+    } else {
+        return 'draw';
+    }
+}
+
+const calculateScore = (prediction, result, scores, index) => {
+    if (result) {
+        let runningScore = 0;
+
+        const winnerOfResult = getWinner(result);
+        const winnerOfPrediction = getWinner(prediction);
+
+        if (winnerOfPrediction === winnerOfResult) {
+            runningScore += 3
+        }
+
+        if (prediction.homeGoals === result.homeGoals) {
+            runningScore += 1
+        }
+
+        if (prediction.awayGoals === result.awayGoals) {
+            runningScore += 1
+        }
+
+        scores[index].score += runningScore;
+    }
+}
+
+export const fetchTally = (results) => {
+    const scores = [
+       { name: 'Hugh', score: 0 },
+       { name: 'Philip', score: 0 },
+       { name: 'Stephen', score: 0 },
+       { name: 'Shane', score: 0 },
+       { name: 'Alan', score: 0 },
+       { name: 'Dylan W', score: 0 },
+       { name: 'Kenny', score: 0 },
+       { name: 'David', score: 0 },
+       { name: 'Dylan C', score: 0 }
+    ];
+
+   results.forEach((result, index) => {
+        calculateScore(hughPredictions[index], result, scores, 0);
+        calculateScore(philipPredictions[index], result, scores, 1);
+        calculateScore(stephenPredictions[index], result, scores, 2);
+        calculateScore(shanePredictions[index], result, scores, 3);
+        calculateScore(alanPredictions[index], result, scores, 4);
+        calculateScore(dylWPredictions[index], result, scores, 5);
+        calculateScore(kennyPredictions[index], result, scores, 6);
+   });
+
+    return scores;
+}
