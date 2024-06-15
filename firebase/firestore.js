c6ee3a5276d7d91447cd0dd88bf7d0beee33b6ee
kenwilde1 <kenwilde@TWRH2JKNQK.local>
2024-06-15 01:08:42 +0100

    correct import

--- firebase/firestore.js
+++ firebase/firestore.js
@@ -1,4 +1,4 @@
-import { getFirestore } from "firebase/firestore";
+import { getFirestore } from "@firebase/firestore";
 import firebaseApp from './firebaseConfig';
 
 const db = getFirestore(firebaseApp);
