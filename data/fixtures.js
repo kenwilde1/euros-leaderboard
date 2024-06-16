435be0b4b422ecdb410e71abaa696f3857737a87
kenwilde1 <kenwilde@TWRH2JKNQK.local>
2024-06-16 10:50:32 +0100

    add more fixtures

--- data/fixtures.js
+++ data/fixtures.js
@@ -8,5 +8,14 @@ export default {
         { id: 'POLNET', time: '14:00', home: 'Poland', away: 'Netherlands'},
         { id: 'SLODEN', time: '17:00', home: 'Slovenia', away: 'Denmark'},
         { id: 'SERENG', time: '20:00', home: 'Serbia', away: 'England'}
-    ]
+    ],
+    17: [
+        { id: 'ROMUKR', time: '14:00', home: 'Romania', away: 'Ukraine'},
+        { id: 'BELSLO', time: '17:00', home: 'Belgium', away: 'Slovakia'},
+        { id: 'AUTFRA', time: '20:00', home: 'Austria', away: 'France'}
+    ],
+    18: [
+        { id: 'TURGEO', time: '17:00', home: 'Turkey', away: 'Georgia'},
+        { id: 'PORCZE', time: '20:00', home: 'Portugal', away: 'Czech Republic'}
+    ],
 }
